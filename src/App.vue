741dbb883b6f25011f1dd00ed92de7c778fd5765
Bhumikkalola21 <bhumik.kalola@bacancy.com>
2023-11-29 18:02:21 +0530

    Remove component folder and add static content line

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>It is Github demo for stream line our vue js project</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
