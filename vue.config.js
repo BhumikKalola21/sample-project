6a2440f5edb5d9b159986df95fae670e8178469c
Bhumikkalola21 <bhumik.kalola@bacancy.com>
2023-11-29 16:22:12 +0530

    changes made in config file

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
+    ? '/https://bhumikkalola21.github.io/sample-project-//'
     : '/'
 })
